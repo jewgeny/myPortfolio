287db674ab364a4611a3e570888c2f88531f01a4
Eugen Kuhn <eugenkuhn@eugens-imac.localdomain>
2019-09-19 17:04:44 +0200

    add cv by about me

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -2,8 +2,17 @@ import React, {Component} from "react";
 import "./css/aboutMe.css";
 import meImg from "./images/me.jpg";
 import {NavLink} from "react-router-dom";
+import ModalPortfolio from "./ModalLebenslauf";
+
 
 class AboutMe extends Component{
+
+   state = {
+    showCV: false
+   }
+   updateModalShow = () => {
+      this.setState(prevState => ({showCV: !prevState.showCV}))
+   }
 
    render(){
   
@@ -21,16 +30,14 @@ class AboutMe extends Component{
                    Seit vielen Jahren interessiere ich mich für die visuelle Gestaltung. Dazu zählen Web -und Printdesign.
                    Ich konnte bereits ein paar Projekte für StartUps und kleineren Unternehmen realisieren.
                    Diese finden Sie bei <NavLink to="/portfolio/own_projects" className="link"> Portfolio</NavLink>.
-                  <br></br><br></br>                  
-                   Seit letztem Jahr (Juni 2018) habe ich die Möglichkeit bekommen bei DCI (Digital Career Institute) eine Umschulung zum Webentwickler zu absolvieren,
-                   welches ich Ende Juli dieses Jahres erfolgreich abschließen konnte.
-                   Ich wollte schon immer wissen, worauf es ankommt,
-                   wenn man von null anfängt eine Webite zu programmieren. Das alles und vieles mehr konnte ich bei DCI erlernen.
-                   Ich möchte mich auch an dieser Stelle meinen tiefsten Dank an meinen damaligen Lehrer Kostas Diakogiannis ausprechen, der
-                   mich jeden Tag mit einer abnormalen Leidenschaft für's Programmieren ermutigt und motiviert hat!
-                   <br/>
+                  <br></br><br></br>
                    Ich liebe was ich tue und ich habe einen hohen Anspruch an Qualität und Funktionalität.
                    Das ist für mich die Voraussetzung für konstant gute Ergebnisse.
+                   <br></br>
+                   <button onClick={this.updateModalShow} className="cvButton">Vollstänger Lebenslauf</button>  
+                   <ModalPortfolio updateModalShow={this.updateModalShow} modalShow={this.state.showCV} />            
+                   
+                  
                  </p>
               </div>
 
---
+++ src/ModalLebenslauf.js
@@ -0,0 +1,106 @@
+import React from "react";
+import { Modal, Button } from 'react-bootstrap';
+import { FaCaretRight } from "react-icons/fa";
+import "./css/cv.css";
+
+
+const ModalPortfolio = (props) => {
+    
+    const cvData = [
+        {arrowIcon: <FaCaretRight />, date: "07/2019", titelDesc: "Einmonatige Vertretung bei NEXMO solution", desc: "Aktualisierung und Erweiterung eines vorhandenen Dashboards, Hamburg" },
+        {arrowIcon: <FaCaretRight />, date: "2017 - 2018", titelDesc: "Atelier für Schrift und Form Schelter", desc: "Grafikdesigner, Münster bei Dieburg"},
+        {arrowIcon: <FaCaretRight />, date: "seit 2014", titelDesc: "Freiberufler als Mediendesigner", desc: "Programmierung von Websites (WordPress, HTML, CSS) und Erstellung diverser Grafiken für den Printbereich, Hamburg"},
+        {arrowIcon: <FaCaretRight />, date: "2011 - 2013", titelDesc: "Body Attack", desc: "Kommissionierer, Hamburg"},
+    ]
+
+    const educationData = [
+        {arrowIcon: <FaCaretRight />, date: "2018 - 2019", titelDesc: "Fernstudium zum Grafikdesigner", desc: "ILS - Institut für Lernsysteme, Hamburg"},
+        {arrowIcon: <FaCaretRight />, date: "2013 - 2015", titelDesc: "Atelier für Schrift und Form Schelter", desc: "Grafikdesigner, Münster bei Dieburg"},
+        {arrowIcon: <FaCaretRight />, date: "2006 - 2007", titelDesc: "Fachabitur", desc: "Fachoberschule G6 - Raum und Objektgestaltung, Hamburg"},
+        {arrowIcon: <FaCaretRight />, date: "2004 - 2006", titelDesc: "Technischer Zeichner", desc: "Schulische Berufsausbildung zum Technischen Zeichner bei Gewerbeschule Metalltechnik, Hamburg"},
+    ]
+
+    const socialData = [
+        {arrowIcon: <FaCaretRight />, date: "2007 - 2008", titelDesc: "Wehrdienst", desc: "2. / Panzerbataillon 515, Kellinghusen"},
+    ]
+
+    return(
+        <Modal
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+                show={props.modalShow}
+                onHide={props.updateModalShow}
+            >
+                <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    Lebenslauf
+                </Modal.Title>
+                </Modal.Header>
+                <Modal.Body className="wholeWrapperCv">
+                <h4 className="titelCV">Beruflicher Werdegang</h4>
+                  {cvData.map((elem, index) => {
+                    return(
+                    <div key={index}>
+                            <div className="blockWrapper">
+                                  <div className="dateArrowWrapper">
+                                    <span className="arrowCV">{elem.arrowIcon}</span>
+                                    <p className="date">{elem.date}</p>
+                                  </div>
+                                <div className="descWrapper">
+                                    <p className="titelDesc">{elem.titelDesc}</p>
+                                    <p className="desc">{elem.desc}</p>
+                                </div>
+                            </div>
+                    </div>
+                    )
+                  })
+                  }
+
+            <h4 className="titelCV">AUSBILDUNG</h4>
+                            {educationData.map((elem, index) => {
+                                return(
+                                <div key={index}>
+                                        <div className="blockWrapper">
+                                            <div className="dateArrowWrapper">
+                                                <span className="arrowCV">{elem.arrowIcon}</span>
+                                                <p className="date">{elem.date}</p>
+                                            </div>
+                                            <div className="descWrapper">
+                                                <p className="titelDesc">{elem.titelDesc}</p>
+                                                <p className="desc">{elem.desc}</p>
+                                            </div>
+                                        </div>
+                                </div>
+                                )
+                            })
+                            }
+                <h4 className="titelCV">SOZIALES ENGAGEMENT</h4>
+                            {socialData.map((elem, index) => {
+                                return(
+                                <div key={index}>
+                                        <div className="blockWrapper">
+                                            <div className="dateArrowWrapper">
+                                                <span className="arrowCV">{elem.arrowIcon}</span>
+                                                <p className="date">{elem.date}</p>
+                                            </div>
+                                            <div className="descWrapper">
+                                                <p className="titelDesc">{elem.titelDesc}</p>
+                                                <p className="desc">{elem.desc}</p>
+                                            </div>
+                                        </div>
+                                </div>
+                                )
+                            })
+                            }
+
+
+                </Modal.Body>
+                <Modal.Footer>
+                <Button onClick={props.updateModalShow}>Close</Button>
+                </Modal.Footer>
+      </Modal>
+    )
+}
+
+export default ModalPortfolio;
